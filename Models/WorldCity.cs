27c5a6e059221d54b4a4568c6792921a64b958ac
Akshaya <akshaya.kini12@gmail.com>
2026-01-28 21:06:40 +0100

    Define WorldCity model

---
+++ Models/WorldCity.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Models;
+
+public class WorldCity
+{
+    [Key]
+    public int CityId { get; set; }
+
+    [Required]
+    [MinLength(2), ErrorMessage("City name must be at least 2 characters long.")]
+    [MaxLength(100), ErrorMessage("City name cannot exceed 100 characters.")]
+    public string CityName { get; set; }
+
+    [Required]
+    [MinLength(2), ErrorMessage("Country name must be at least 2 characters long.")]
+    [MaxLength(100), ErrorMessage("Country name cannot exceed 100 characters.")]
+    public string Country { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue), ErrorMessage("Population must be a non-negative integer.")]
+    public int Population { get; set; }
+}
