bca41eda0613464d7b98f1922d5f39dddaef243e
Akshaya <akshaya.kini12@gmail.com>
2026-01-29 10:23:46 +0100

    Create DTOs for WorldCity

---
+++ WebAPIWorldCities/DTOs/WorldCityDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPIWorldCities.DTOs;
+
+public class WorldCityDto
+{
+    public int CityId { get; set; }
+
+    public string CityName { get; set; }
+
+   public string Country { get; set; }
+
+    public int Population { get; set; }
+}
